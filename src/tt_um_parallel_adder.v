52ea042630926963c7e45e19bbd6c89caf1ec65b
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-08-23 20:24:50 +0530

    Update tt_um_parallel_adder.v

--- src/tt_um_parallel_adder.v
+++ src/tt_um_parallel_adder.v
@@ -1,9 +1,9 @@
 module tt_um_parallel_adder #( parameter MAX_COUNT = 8 ) (
-    input  wire [MAX_COUNT-1:0] ui_in1,    // Dedicated inputs A
-    input  wire [MAX_COUNT-1:0] ui_in2,    // Dedicated inputs B
-    input  wire ui_in3,    // Dedicated input Cin
-    output wire [MAX_COUNT-1:0] uo_out1,   // Dedicated outputs Sum
-    output wire uo_out2   // Dedicated output Cout
+    input  wire [MAX_COUNT-1:0] ui_in,    // input A
+    input  wire [MAX_COUNT-1:0] uio_in,    // input B
+    input  wire uio_oe,    // input Cin
+    output wire [MAX_COUNT-1:0] uo_out,   // output Sum
+    output wire uio_out   // output Cout
     // input  wire [MAX_COUNT-1:0] uio_in,   // IOs: Input path
     // output wire [MAX_COUNT-1:0] uio_out,  // IOs: Output path
     // output wire [MAX_COUNT-1:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -16,19 +16,19 @@ wire [7:0] sum_bits;
 wire [6:0] carry;
 wire c_out;
 
-    fulladder fa0 (.A(ui_in1[0]), .B(ui_in2[0]), .Cin(ui_in3), .Sum(sum_bits[0]), .Cout(carry[0]));
+    fulladder fa0 (.A(ui_in[0]), .B(uio_in[0]), .Cin(uio_oe), .Sum(sum_bits[0]), .Cout(carry[0]));
 genvar i;
 generate
       for (i=1; i < 7; i=i+1)
       begin
-            fulladder fa (.A(ui_in1[i]), .B(ui_in2[i]), .Cin(carry[i-1]), .Sum(sum_bits[i]), .Cout(carry[i]));
+            fulladder fa (.A(ui_in[i]), .B(uio_in[i]), .Cin(carry[i-1]), .Sum(sum_bits[i]), .Cout(carry[i]));
       end
    endgenerate
-fulladder fa7 (.A(ui_in1[7]), .B(ui_in2[7]), .Cin(carry[6]), .Sum(sum_bits[7]), .Cout(c_out));
+fulladder fa7 (.A(ui_in[7]), .B(uio_in[7]), .Cin(carry[6]), .Sum(sum_bits[7]), .Cout(c_out));
 
 always @ (*) begin
-    uo_out1 <= sum_bits;
-    uo_out2 <= c_out;
+    uo_out <= sum_bits;
+    uio_out <= c_out;
 end
 
 endmodule
