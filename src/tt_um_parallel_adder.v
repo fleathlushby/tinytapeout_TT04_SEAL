893ebabc0bda4cdb8840617de69fd5b5bfe5d812
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-08-23 19:40:29 +0530

    Update tt_um_parallel_adder.v

--- src/tt_um_parallel_adder.v
+++ src/tt_um_parallel_adder.v
@@ -1,27 +1,34 @@
-module parallel_adder (
-    input [7:0] A,
-    input [7:0] B,
-    input Cin,
-    output reg [7:0] Sum,
-    output reg Cout
+module tt_um_parallel_adder #( parameter MAX_COUNT = 8 ) (
+    input  wire [MAX_COUNT-1:0] ui_in1,    // Dedicated inputs A
+    input  wire [MAX_COUNT-1:0] ui_in2,    // Dedicated inputs B
+    input  wire ui_in3,    // Dedicated input Cin
+    output wire [MAX_COUNT-1:0] uo_out1,   // Dedicated outputs Sum
+    output wire uo_out2   // Dedicated output Cout
+    // input  wire [MAX_COUNT-1:0] uio_in,   // IOs: Input path
+    // output wire [MAX_COUNT-1:0] uio_out,  // IOs: Output path
+    // output wire [MAX_COUNT-1:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    // input  wire       ena,      // will go high when the design is enabled
+    // input  wire       clk,      // clock
+    // input  wire       rst_n     // reset_n - low to reset
 );
 
 wire [7:0] sum_bits;
-wire [7:0] carry;
+wire [6:0] carry;
 wire c_out;
 
-fulladder fa0 (.A(A[0]), .B(B[0]), .Cin(Cin), .Sum(sum_bits[0]), .Cout(carry[0]));
-fulladder fa1 (.A(A[1]), .B(B[1]), .Cin(carry[0]), .Sum(sum_bits[1]), .Cout(carry[1]));
-fulladder fa2 (.A(A[2]), .B(B[2]), .Cin(carry[1]), .Sum(sum_bits[2]), .Cout(carry[2]));
-fulladder fa3 (.A(A[3]), .B(B[3]), .Cin(carry[2]), .Sum(sum_bits[3]), .Cout(carry[3]));
-fulladder fa4 (.A(A[4]), .B(B[4]), .Cin(carry[3]), .Sum(sum_bits[4]), .Cout(carry[4]));
-fulladder fa5 (.A(A[5]), .B(B[5]), .Cin(carry[4]), .Sum(sum_bits[5]), .Cout(carry[5]));
-fulladder fa6 (.A(A[6]), .B(B[6]), .Cin(carry[5]), .Sum(sum_bits[6]), .Cout(carry[6]));
-fulladder fa7 (.A(A[7]), .B(B[7]), .Cin(carry[6]), .Sum(sum_bits[7]), .Cout(c_out));
+    fulladder fa0 (.A(ui_in1[0]), .B(ui_in2[0]), .Cin(ui_in3), .Sum(sum_bits[0]), .Cout(carry[0]));
+genvar i;
+generate
+      for (i=1; i < 7; i=i+1)
+      begin
+            fulladder fa (.A(ui_in1[i]), .B(ui_in2[i]), .Cin(carry[i-1]), .Sum(sum_bits[i]), .Cout(carry[i]));
+      end
+   endgenerate
+fulladder fa7 (.A(ui_in1[7]), .B(ui_in2[7]), .Cin(carry[6]), .Sum(sum_bits[7]), .Cout(c_out));
 
 always @ (*) begin
-    Sum <= sum_bits;
-    Cout <= c_out;
+    uo_out1 <= sum_bits;
+    uo_out2 <= c_out;
 end
 
 endmodule
