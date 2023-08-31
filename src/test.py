05fea24878e6c7d7fbca97a0cac19485975c81eb
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-08-31 20:27:43 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,49 +2,25 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+input_A = [0,1,2,3,4,5,6,7,5,6]
+input_B = [7,6,5,4,3,2,1,0,5,7]
+input_Cin = 0
 
 @cocotb.test()
-async def rca_adder(dut):
+async def test_rca(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # reset
     dut._log.info("reset")
     dut.rst.value = 1
-    # set the compare value
-    dut.A.value = 7
-    dut.B.value = 5
-    dut.Cin.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    # max_count = dut.ui_in.value << 10
-    # dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    # for i in range(15):
-    #     dut._log.info("check segment {}".format(i))
-    #     await ClockCycles(dut.clk, max_count)
-    #     assert int(dut.segments.value) == segments[i % 10]
-
-        # all bidirectionals are set to output
-        # assert dut.uio_oe == 0xFF
-
-    # reset
-    dut.rst.value = 1
-    # set a different compare value
-    dut.A.value = 3
-    dut.B.value = 6
-    dut.Cin.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst.value = 0
-
-    # max_count = dut.ui_in.value << 10
-    # dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    # for i in range(15):
-    #     dut._log.info("check segment {}".format(i))
-    #     await ClockCycles(dut.clk, max_count)
-    #     assert int(dut.segments.value) == segments[i % 10]
-
+    dut._log.info("check 10 inputs")
+    for i in range(10):
+        dut._log.info("check input {}".format(i))
+        await ClockCycles(dut.clk, 1000)
+        assert int(dut.A.value) == input_A[i]
+        assert int(dut.B.value) == input_B[i]
+        assert int(dut.Cin.value) == input_Cin
