91b5569a666faf43b5161a4f624908224d9b39bb
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 10:17:13 +0530

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -29,12 +29,14 @@ module tb ();
     // wire [2:0] Sum;
     // wire Cout;
 
-    tt_um_parallel_adder #(.MAX_COUNT(1000)) tt_um_parallel_adder (
+    
+    
+    tt_um_parallel_adder tt_um_parallel_adder(
     `ifdef GL_TEST
     // include power ports for the Gate Level test
         .VPWR( 1'b1),
         .VGND( 1'b0),
-    `else
+    `endif
         .ui_in      (ui_in),    // Dedicated inputs
         .uo_out     (uo_out),   // Dedicated outputs
         .uio_in     (uio_in),   // IOs: Input path
@@ -44,5 +46,4 @@ module tb ();
         .clk        (clk),      // clock
         .rst_n      (rst_n)     // not reset
         );
-    `endif
 endmodule
